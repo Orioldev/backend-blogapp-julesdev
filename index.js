0dcc8e3ae8c9f5afd3ed6d9a423c7bd836b38267
Juberson Jules <76498996+Orioldev@users.noreply.github.com>
2024-02-14 22:57:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.use( errorHandler )
 
 
 
-connect(process.env.MONGO_URI).then(app.listen( process.env.PORT || 5000, () => console.log(`Server started on port ${process.env.PORT}`) )).catch( error => { console.log( error ) } )
+connect(process.env.MONGO_URI).then(app.listen( process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`) )).catch( error => { console.log( error ) } )
 
